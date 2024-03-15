d7bd12e443b90fe7ae71b2d047dd52c9de8e1ec7
Gabriel Maciel Zavarize <gabrielmzavarize@gmail.com>
2024-03-14 21:00:01 -0300

    2 exer 14/03
    Mais dois exercicios feitos hoje em Python

---
+++ exemformat.py
@@ -0,0 +1,4 @@
+nome = "Gabriel"
+sobrenome = "Maciel"
+
+print ("Meu nome é {} e meu sobrenome é {}".format(nome, sobrenome))
---
+++ separador.py
@@ -0,0 +1,6 @@
+data = input("Digite uma data no formato DD/MM/AAAA: ")
+dia, mes, ano = data.split('/')
+print("Dia:", dia)
+print("Mês:", mes)
+print("Ano:", ano)
+
